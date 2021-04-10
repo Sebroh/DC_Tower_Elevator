b301edb2ea4e466cbd32b8f2f7a153560bf00040
Sebastian <Sebastian@DESKTOP-73HU1JU>
2021-04-10 20:54:47 +0200

    Add TestCases and Output and refactor Code

--- src/main/java/DCTower.java
+++ src/main/java/DCTower.java
@@ -16,34 +16,40 @@ import java.util.concurrent.BlockingQueue;
 public class DCTower {
     BlockingQueue<Access> requests = new ArrayBlockingQueue(1024);
     
-    void addQueue(Access a){
+    void addQueue(Access a) {
         requests.add(a);
     }
     
-    BlockingQueue<Access> getQueue(){
+    BlockingQueue<Access> getQueue() {
         return this.requests;
     }
     
     DCTower() {
         //create 7 Threads for each Elevator
-        for(int i = 1; i < 2; i++){
+        for (int i = 1; i < 8; i++) {
             new Thread(new Elevator(this.requests)).start();
         }
-        
     }
     
     /**
      * @param args the command line arguments
      */
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         DCTower tower = new DCTower();
         
-        Access a1 = new Access(1, 10, Direction.UP);
-        Access a2 = new Access(3, 13, Direction.UP);
+        tower.addQueue(new Access(1, 10, Direction.UP));
+        tower.addQueue(new Access(3, 13, Direction.UP));
+        tower.addQueue(new Access(18, 8, Direction.DOWN));
+        tower.addQueue(new Access(35, 0, Direction.DOWN));
+        Thread.sleep(4000);
+        tower.addQueue(new Access(54, 4, Direction.DOWN));
+        tower.addQueue(new Access(0, 48, Direction.UP));
+        Thread.sleep(12000);
+        tower.addQueue(new Access(5, 19, Direction.UP));
+        tower.addQueue(new Access(37, 9, Direction.DOWN));
         
-        tower.addQueue(a1);
-        tower.addQueue(a2);
+        
     }
     
 }
