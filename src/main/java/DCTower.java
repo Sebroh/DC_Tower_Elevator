9e7b1b6ad8c4f021eefa537d2ec2f99eb60edccb
Sebastian <Sebastian@DESKTOP-73HU1JU>
2021-04-10 23:00:22 +0200

    Add exit after Program finishes

--- src/main/java/DCTower.java
+++ src/main/java/DCTower.java
@@ -1,5 +1,4 @@
-
-import java.util.ArrayList;
+import java.util.Set;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
@@ -11,9 +10,12 @@ import java.util.concurrent.BlockingQueue;
 
 /**
  *
- * @author Sebastian
+ * @author Sebastian Rohrer
  */
 public class DCTower {
+    
+    //stores all requests from the user
+    //is a thread safe queue, to provide access to multiple threads at once
     BlockingQueue<Access> requests = new ArrayBlockingQueue(1024);
     
     void addQueue(Access a) {
@@ -25,23 +27,31 @@ public class DCTower {
     }
     
     DCTower() {
-        //create 7 Threads for each Elevator
+        //creates 7 Threads for each Elevator
         for (int i = 1; i < 8; i++) {
-            new Thread(new Elevator(this.requests)).start();
+            new Thread(new Elevator(this.requests), String.valueOf(i)).start();
         }
     }
+      
     
     /**
      * @param args the command line arguments
+     * @throws java.lang.InterruptedException
      */
     
     public static void main(String[] args) throws InterruptedException {
         DCTower tower = new DCTower();
         
+        //add Elevator Requests
         tower.addQueue(new Access(1, 10, Direction.UP));
         tower.addQueue(new Access(3, 13, Direction.UP));
         tower.addQueue(new Access(18, 8, Direction.DOWN));
         tower.addQueue(new Access(35, 0, Direction.DOWN));
+        tower.addQueue(new Access(41, 45, Direction.UP));
+        tower.addQueue(new Access(0, 55, Direction.UP));
+        tower.addQueue(new Access(21, 45, Direction.UP));
+        tower.addQueue(new Access(12, 0, Direction.DOWN));
+        tower.addQueue(new Access(12, 32, Direction.UP));
         Thread.sleep(4000);
         tower.addQueue(new Access(54, 4, Direction.DOWN));
         tower.addQueue(new Access(0, 48, Direction.UP));
@@ -50,6 +60,8 @@ public class DCTower {
         tower.addQueue(new Access(37, 9, Direction.DOWN));
         
         
+        Thread.sleep(10000);
+        System.exit(0);  
     }
     
 }
