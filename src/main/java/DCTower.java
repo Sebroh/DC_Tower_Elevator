1a2302664e822c77a2ccb1d38f81af38e8d7e98d
Sebastian <Sebastian@DESKTOP-73HU1JU>
2021-04-10 15:33:37 +0200

    Add BlockingQueue and Elevator Test Example

--- src/main/java/DCTower.java
+++ src/main/java/DCTower.java
@@ -1,3 +1,8 @@
+
+import java.util.ArrayList;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,13 +14,34 @@
  * @author Sebastian
  */
 public class DCTower {
-
+    BlockingQueue<Access> requests = new ArrayBlockingQueue(1024);
+    
+    void addQueue(Access a){
+        requests.add(a);
+    }
+    
+    BlockingQueue<Access> getQueue(){
+        return this.requests;
+    }
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
-        Access getBüro = new Access(0, 35, Direction.UP);
-        System.out.println("Want to Office from " + getBüro.getCurr() + " Floor");
+        DCTower tower = new DCTower();
+        
+        Access a1 = new Access(1, 10, Direction.UP);
+        Access a2 = new Access(10, 1, Direction.DOWN);
+        
+        tower.addQueue(a1);
+        tower.addQueue(a2);
+        
+        Elevator e1 = new Elevator(tower.getQueue());
+        Elevator e2 = new Elevator(tower.getQueue());
+      
+        new Thread(e1).start();
+        new Thread(e2).start();
+        
     }
     
 }
