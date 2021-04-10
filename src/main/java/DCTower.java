f3f8a8ecbf526c0283df957f26e44e68b819fc69
Sebastian <Sebastian@DESKTOP-73HU1JU>
2021-04-10 15:40:06 +0200

    Add 7 Elevators

--- src/main/java/DCTower.java
+++ src/main/java/DCTower.java
@@ -38,9 +38,19 @@ public class DCTower {
         
         Elevator e1 = new Elevator(tower.getQueue());
         Elevator e2 = new Elevator(tower.getQueue());
+        Elevator e3 = new Elevator(tower.getQueue());
+        Elevator e4 = new Elevator(tower.getQueue());
+        Elevator e5 = new Elevator(tower.getQueue());
+        Elevator e6 = new Elevator(tower.getQueue());
+        Elevator e7 = new Elevator(tower.getQueue());
       
         new Thread(e1).start();
         new Thread(e2).start();
+        new Thread(e3).start();
+        new Thread(e4).start();
+        new Thread(e5).start();
+        new Thread(e6).start();
+        new Thread(e7).start();
         
     }
     
