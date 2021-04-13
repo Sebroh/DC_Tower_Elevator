14b665a8f6f8ea9271df136fbe25dcf1381278fe
Sebastian <Sebastian@DESKTOP-73HU1JU>
2021-04-13 12:11:49 +0200

    Change author and header

--- src/main/java/DCTower.java
+++ src/main/java/DCTower.java
@@ -2,12 +2,6 @@ import java.lang.Thread.State;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author Sebastian Rohrer
